e60db4e6297854275b6a3d43666619441471dab3
findasifrahman <findasifrahman@gmail.com>
2021-07-23 22:04:05 +0600

    Roll_of_events_Backebd_2

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID:"454309964105-l16t5kj91a62okok9smjs8ks4tn7jsgf.apps.googleusercontent.com",
-        clientSecret:"hr8HAIn3-1OKrRymSFbq2TNY"
-    }
-}
